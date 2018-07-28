4846d7069277d05f2bd178e02c46f55058f4594a
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-07-28 21:10:37 +0700

    Update replypush.php

--- replypush.php
+++ replypush.php
@@ -45,16 +45,11 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
+
 if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$userID = $event['source']['userId'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-      
+ 			$userID = $event['source']['userId'];
+ 			$replyToken = $event['replyToken'];
+}     
 
 $channelSecret =  '5ee439e1f6308d8a4a660f944c2ce8e5';  
   // '75c03f392f6e53d662d6f5a8db9e421f';
