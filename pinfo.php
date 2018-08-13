fde8717d9cfd89db458cb24d8dae90f47078521b
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-08-13 10:53:34 +0700

    Update pinfo.php

--- pinfo.php
+++ pinfo.php
@@ -1,3 +1,18 @@
 <?php 
-phpinfo();
+
+$m = new Memcached();
+$m->addServer('localhost', 11211);
+
+$m->set('int', 99);
+$m->set('string', 'a simple string');
+$m->set('array', array(11, 12));
+/* expire 'object' key in 5 minutes */
+$m->set('object', new stdclass, time() + 300);
+
+
+var_dump($m->get('int'));
+var_dump($m->get('string'));
+var_dump($m->get('array'));
+var_dump($m->get('object'));phpinfo();
+
 ?>
