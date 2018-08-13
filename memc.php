200b56df0c71b1fec2934c01071f904530501da1
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-08-13 10:28:58 +0700

    Update memc.php

--- memc.php
+++ memc.php
@@ -7,10 +7,13 @@ $memtest = new Memcached();
 //$memtest->addServer("memcached-11878.c14.us-east-1-2.ec2.cloud.redislabs.com", 11878);
 
 $memtest->addServer("127.0.0.1", 11878);
- 
+$memtest->set("Bilbo", "สุดยอด"); 
 $response = $mem_var->get("Bilbo");
+echo $response;
+
 if ($response) {
 &nbsp; &nbsp; echo $response;
+ 
 } else {
 &nbsp; &nbsp; echo "Adding Keys (K) for Values (V), You can then grab Value (V) for your Key (K) \m/ (-_-) \m/ ";
 &nbsp; &nbsp; $mem_var->set("Bilbo", "Here s Your (Ring) Master stored in MemCached (^_^)") or die(" Keys Couldn't be Created : Bilbo Not Found :'( ");
