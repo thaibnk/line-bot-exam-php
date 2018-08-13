b3c5e5dffb603fbaaacc06aea72626df83509bb2
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-08-13 11:30:48 +0700

    Update pinfo.php

--- pinfo.php
+++ pinfo.php
@@ -1,5 +1,11 @@
 <?php 
 
+     connect(‘localhost’, 11211) or die (“Could not connect”);
+
+// ติดต่อกับ server โดย localhost และ 11211 คือ port สำหรับติดต่อ memcache
+
+    echo “Version : “.$memcache->getVersion();
+
 $m = new Memcached();
 $m->addServer('localhost', 11211);
 
