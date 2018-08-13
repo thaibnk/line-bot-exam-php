7f325e2649c8168995ac4a5f0c75dc4209deb9f6
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-08-13 11:49:12 +0700

    Update mctest.php

--- mctest.php
+++ mctest.php
@@ -1,7 +1,7 @@
 <?php
 
 $memcache = new Memcache;
-$memcache->connect('localhost',11878) or die ("Could not connect");
+$memcache->connect('localhost',11211) or die ("Could not connect");
 
 $version = $memcache->getVersion();
 echo "Server's version: ".$version."<br/>\n";
