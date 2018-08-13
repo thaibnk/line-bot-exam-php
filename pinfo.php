1b9858c0939ef9127877036a15a2a92ae09eaaca
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-08-13 11:36:56 +0700

    Update pinfo.php

--- pinfo.php
+++ pinfo.php
@@ -1,10 +1,13 @@
 <?php 
+require 'vendor/autoload.php';
 
-     connect(‘localhost’, 11211) or die (“Could not connect”);
+// create a new persistent client
+$m = new Memcached("memcached_pool");
+//connect(‘localhost’, 11211) or die (“Could not connect”);
 
 // ติดต่อกับ server โดย localhost และ 11211 คือ port สำหรับติดต่อ memcache
 
-    echo “Version : “.$memcache->getVersion();
+   // echo “Version : “.$memcache->getVersion();
 
 
 
