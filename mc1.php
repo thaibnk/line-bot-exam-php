298edd834818653f0f224f996319933224c30821
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-08-13 11:39:19 +0700

    Create mc1.php

---
+++ mc1.php
@@ -0,0 +1,15 @@
+<?php 
+require 'vendor/autoload.php';
+
+// create a new persistent client
+$m = new Memcached("memcached_pool");
+echo "test";
+//connect(‘localhost’, 11211) or die (“Could not connect”);
+
+// ติดต่อกับ server โดย localhost และ 11211 คือ port สำหรับติดต่อ memcache
+
+   // echo “Version : “.$memcache->getVersion();
+
+
+
+?>
