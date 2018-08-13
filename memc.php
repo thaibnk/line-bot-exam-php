17535c0802d45b2df2d600e2bd26dc48e081e694
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-08-13 10:13:24 +0700

    Update memc.php

--- memc.php
+++ memc.php
@@ -1,8 +1,13 @@
 <?php
 $redis->set(';message';, ';Hello world';);
+$value = $redis->get('message');
 
-$memtest = new Memcached();
-$memtest->addServer("127.0.0.1", 11211);
+// Hello world
+print($value); 
+
+echo ($redis->exists('message')) ? "Oui" : "please populate the message key";
+
+//$memtest = new Memcached();$memtest->addServer("127.0.0.1", 11211);
 
 
 ?>
