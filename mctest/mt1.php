16890ab2671a22c391e0c93a925650fd14e6ed1e
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-08-14 10:58:11 +0700

    Update mt1.php

--- mctest/mt1.php
+++ mctest/mt1.php
@@ -2,8 +2,25 @@
 error_reporting(E_ALL & ~E_NOTICE);
 
 //error_reporting(E_ALL & ~E_NOTICE);
+$memcache = new Memcached ( 'a_mem_pool' );
+$ss = $memcache->getServerList ();
+if (empty ( $ss )) {
+$memcache->setOption(Memcached::OPT_RECV_TIMEOUT, 1000);
+$memcache->setOption(Memcached::OPT_SEND_TIMEOUT, 1000);
+$memcache->setOption(Memcached::OPT_TCP_NODELAY, true);
+$memcache->setOption(Memcached::OPT_SERVER_FAILURE_LIMIT, 50);
+$memcache->setOption(Memcached::OPT_CONNECT_TIMEOUT, 500);
+$memcache->setOption(Memcached::OPT_RETRY_TIMEOUT, 300);
+$memcache->setOption(Memcached::OPT_DISTRIBUTION, Memcached::DISTRIBUTION_CONSISTENT);
+$memcache->setOption(Memcached::OPT_REMOVE_FAILED_SERVERS, true);
+$memcache->setOption(Memcached::OPT_LIBKETAMA_COMPATIBLE, true);
+$memcache->addServer ( '10.10.1.75', 11211, 1 );
+$memcache->addServer ( '10.10.1.76', 11211, 1 );
+$memcache->addServer ( '10.10.1.77', 11211, 1 );
+}
 
-$mc = new Memcached('memcached-app103432799');
+/*
+$mc = new Memcached('a');
 $mc->addServer("localhost", 11211);
 
 $mc->set("foo", "Hello!");
