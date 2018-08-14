5b4927f6e4c5a60daf74e02e564c991363855639
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-08-14 16:20:34 +0700

    Update mc1.php

--- mc1.php
+++ mc1.php
@@ -1,9 +1,14 @@
 <?php
 echo "สวัสดี";
+
 $mc = new Memcached();
 $mc->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
-$mc->addServers(array_map(function($server) { return explode(':', $server, 2); }, explode(',', $_ENV['MEMCACHEDCLOUD_SERVERS'])));
-$mc->setSaslAuthData($_ENV['MEMCACHEDCLOUD_USERNAME'], $_ENV['MEMCACHEDCLOUD_PASSWORD']);
+
+//$mc->addServers(array_map(function($server) { return explode(':', $server, 2); }, explode(',', $_ENV['MEMCACHEDCLOUD_SERVERS']) )    );
+$mc->addServers('memcached-11878.c14.us-east-1-2.ec2.cloud.redislabs.com',11878);
+
+//$mc->setSaslAuthData($_ENV['MEMCACHEDCLOUD_USERNAME'], $_ENV['MEMCACHEDCLOUD_PASSWORD']);
+$mc->setSaslAuthData('memcached-app103432799', '7rKlQcdY6FeJT8kf07pA2ULedG8DZTX4');
 
 switch ($_GET['a']) {
   case 'set':
