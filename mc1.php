5c68889f65248e2c91be943276a4a8b88f2c8ea9
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-08-14 13:48:17 +0700

    Update mc1.php

--- mc1.php
+++ mc1.php
@@ -1,13 +1,28 @@
-<?php 
-require 'vendor/autoload.php';
+<?php
+$mc = new Memcached();
+$mc->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
+$mc->addServers(array_map(function($server) { return explode(':', $server, 2); }, explode(',', $_ENV['MEMCACHEDCLOUD_SERVERS'])));
+$mc->setSaslAuthData($_ENV['MEMCACHEDCLOUD_USERNAME'], $_ENV['MEMCACHEDCLOUD_PASSWORD']);
 
-// create a new persistent client
-//$m = new Memcached("memcached_pool");
-echo "test";
-//connect(‘localhost’, 11211) or die (“Could not connect”);
-
-// ติดต่อกับ server โดย localhost และ 11211 คือ port สำหรับติดต่อ memcache
-
-   // echo “Version : “.$memcache->getVersion();
-
+switch ($_GET['a']) {
+  case 'set':
+    echo $mc->set('welcome_msg', 'Hello from Redis!');
+    break;
+  case 'get':
+    echo $mc->get('welcome_msg');
+    break;
+  case 'stats':
+    foreach ($mc->getStats() as $key => $value) {
+      foreach ($value as $k => $v) {
+        echo "$k: $v <br />\n";
+      }
+    }
+    break;
+  case 'delete':
+    echo $mc->delete('welcome_msg');
+    break;
+  default:
+    echo '';
+    break;
+}
 ?>
