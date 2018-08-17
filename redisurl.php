1eb21fae6a5896c291bbbbe56efad9985019d673
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-08-17 13:17:46 +0700

    Update redisurl.php

--- redisurl.php
+++ redisurl.php
@@ -1,13 +1,15 @@
 <?php
   //edis = new Redis(); 
   //redis->connect("redis://h:p94a00b50a567eef4d65cb6230d498f1603ecdb5eac09324791a4c77f4a0a20f5@ec2-18-214-19-12.compute-1.amazonaws.com:15009, 6379); 
-   new RedisURI("redis://h:p94a00b50a567eef4d65cb6230d498f1603ecdb5eac09324791a4c77f4a0a20f5@ec2-18-214-19-12.compute-1.amazonaws.com:15009", 6379, 60, TimeUnit.SECONDS);
-  $value = $redis->get('foo');
+  // new RedisURI("redis://h:p94a00b50a567eef4d65cb6230d498f1603ecdb5eac09324791a4c77f4a0a20f5@ec2-18-214-19-12.compute-1.amazonaws.com:15009", 6379, 60, TimeUnit.SECONDS);
+ 
 echo "Connection to server sucessfully".$value
 /*
-$redis = new redis\Client($_ENV('REDIS_URL'));
-echo "ทดสอบ redis".getenv('REDIS_URL');
+$redis = new redis\Client($_ENV['REDIS_URL']);
+echo "ทดสอบ redis".$_ENV['REDIS_URL'];
 
+ $value = $redis->get('foo');
+ 
 //$redis->set(';message';, ';Hello world';);
 
 // gets the value of message
