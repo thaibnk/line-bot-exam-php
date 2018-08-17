3a126027f44c47c14bd71c923b5da33f2847a0e0
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-08-17 12:43:24 +0700

    Update redisurl.php

--- redisurl.php
+++ redisurl.php
@@ -1,7 +1,9 @@
 <?php
-  $redis = new Redis(); 
-   $redis->connect('127.0.0.1', 6379); 
-   echo "Connection to server sucessfully"; 
+  //edis = new Redis(); 
+  //redis->connect("redis://h:p94a00b50a567eef4d65cb6230d498f1603ecdb5eac09324791a4c77f4a0a20f5@ec2-18-214-19-12.compute-1.amazonaws.com:15009, 6379); 
+   new RedisURI("redis://h:p94a00b50a567eef4d65cb6230d498f1603ecdb5eac09324791a4c77f4a0a20f5@ec2-18-214-19-12.compute-1.amazonaws.com:15009", 6379, 60, TimeUnit.SECONDS);
+  $value = $redis->get('foo');
+echo "Connection to server sucessfully".$value
 /*
 $redis = new redis\Client($_ENV('REDIS_URL'));
 echo "ทดสอบ redis".getenv('REDIS_URL');
