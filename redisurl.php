a38979a9587002966e93f53d209acc2f31116230
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-08-17 12:33:23 +0700

    Update redisurl.php

--- redisurl.php
+++ redisurl.php
@@ -1,12 +1,15 @@
 <?php
 echo "ทดสอบ redis <br>";
 $redis = new Predis\Client(getenv('REDIS_URL'));
-$redis->set(';message';, ';Hello world';);
+//$redis->set(';message';, ';Hello world';);
 
 // gets the value of message
-$value = $redis->get('message');
+//$value = $redis->get('message');
+
+$value=$redis->get('foo');
 
 // Hello world
-print('สวัสดี ='.$value); 
-
-echo 'a='.($redis->exists('message')) ? "Oui" : "please populate the message key";
+//print('สวัสดี ='.$value); 
+echo $value;
+//echo 'a='.($redis->exists('message')) ? "Oui" : "please populate the message key";
+?>
