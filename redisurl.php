ea6e8535a02a1cf3adc3e043269450d28367b96f
thaibnk <41515698+thaibnk@users.noreply.github.com>
2018-08-17 13:22:26 +0700

    Update redisurl.php

--- redisurl.php
+++ redisurl.php
@@ -1,9 +1,10 @@
 <?php
+require "vendor/autoload.php";
   //edis = new Redis(); 
   //redis->connect("redis://h:p94a00b50a567eef4d65cb6230d498f1603ecdb5eac09324791a4c77f4a0a20f5@ec2-18-214-19-12.compute-1.amazonaws.com:15009, 6379); 
   // new RedisURI("redis://h:p94a00b50a567eef4d65cb6230d498f1603ecdb5eac09324791a4c77f4a0a20f5@ec2-18-214-19-12.compute-1.amazonaws.com:15009", 6379, 60, TimeUnit.SECONDS);
  
-echo "Connection to server sucessfully".$value
+echo "Connection to server sucessfully";
 
 $redis = new redis\Client($_ENV['REDIS_URL']);
 echo "ทดสอบ redis".$_ENV['REDIS_URL'];
